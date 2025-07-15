2a9bca32686ec48d32e057cc3623a2cb3546d304
anyozhik <ateslyuk5@gmail.com>
2025-07-15 15:10:52 +0700

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,9 +1,11 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
+
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
     registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
@@ -18,9 +20,16 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    main_page_header = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(main_page_header).to_be_visible()
-    expect(main_page_header).to_have_text('Dashboard')
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+
+    page = context.new_page()
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    page.wait_for_timeout(5000)
+
 
 
 
