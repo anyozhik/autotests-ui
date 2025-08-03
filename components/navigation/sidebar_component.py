e81dec4b876cfa6d6957cf67a0f5a8e2bae7c76b
anyozhik <ateslyuk5@gmail.com>
2025-08-03 15:16:54 +0700

    Практические применение PageFactory в автотестах

--- components/navigation/sidebar_component.py
+++ components/navigation/sidebar_component.py
@@ -1,8 +1,6 @@
 import re
-
 from components.base_component import BaseComponent
 from playwright.sync_api import Page
-
 from components.navigation.sidebar_list_item_component import SidebarListItemComponent
 
 
