03162a8bcc8618f52b874d63ec5d08dee85e701a
anyozhik <ateslyuk5@gmail.com>
2025-08-11 23:49:05 +0700

    Запуск автотестов на разных браузерах

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -7,9 +7,11 @@ from config import settings
 from tools.routes import AppRoute
 
 
-@pytest.fixture
-def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+@pytest.fixture(params=settings.browsers)
+def page(request: SubRequest, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(playwright,
+                                          test_name=request.node.name,
+                                          browser_type=request.param)
 
 
 @pytest.fixture(scope="session")
@@ -31,9 +33,13 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state=settings.browser_state_file)
+@pytest.fixture(params=settings.browsers)
+def page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(playwright,
+                                          test_name=request.node.name,
+                                          browser_type=request.param,
+                                          storage_state=settings.browser_state_file
+                                          )
 
 
 
